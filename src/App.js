e74ada1bfc604cbf0339d62a30112ff9956a2dc0
JeffAllison <itsjallison@gmail.com>
2022-05-16 17:31:05 -0700

    added url image from API to props to be mapped through, added img tag to be rendered

--- src/App.js
+++ src/App.js
@@ -40,18 +40,21 @@ function App() {
         {loadingSpinner ? (
           <LoadingSpinner />
         ) : (
-          pokemon.map(({ pokemon, height, weight, hp, shape, type_1, type_2, ability_1 }, i) => (
-            <div className="pokemon" key={pokemon + i}>
-              <h3>Name: {pokemon}</h3>
-              <p>Height: {height}</p>
-              <p>Weight: {weight}</p>
-              <p>Health: {hp}</p>
-              <p>Shape: {shape}</p>
-              <p>Type 1: {type_1}</p>
-              <p>Type 2: {type_2}</p>
-              <p>Ability 1: {ability_1}</p>
-            </div>
-          ))
+          pokemon.map(
+            ({ pokemon, height, weight, hp, shape, type_1, type_2, ability_1, url_image }, i) => (
+              <div className="pokemon" key={pokemon + i}>
+                <h3>Name: {pokemon}</h3>
+                <p>Height: {height}</p>
+                <p>Weight: {weight}</p>
+                <p>Health: {hp}</p>
+                <p>Shape: {shape}</p>
+                <p>Type 1: {type_1}</p>
+                <p>Type 2: {type_2}</p>
+                <p>Ability 1: {ability_1}</p>
+                <img src={url_image} />
+              </div>
+            )
+          )
         )}
       </header>
     </div>
